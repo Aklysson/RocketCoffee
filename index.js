34709d1cc9fe46e78da63226d5d22b8d7f1acc88
Aklysson Roberto <aklyroberto@hotmail.com>
2022-07-14 09:37:02 -0300

    Adding X button

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const btnMobile = document.getElementById("btnmobile");
+const closingBtn = document.getElementById("close-btn");
 
-function cliquei() {
+
+function handleClick() {
     const nav = document.getElementById('nav')
+    const close =document.getElementById("close-btn");
+    const open =document.getElementById("btnmobile");
+
     nav.classList.toggle('active')
+    close.classList.toggle('active');
+    open.classList.toggle('active');
+  
 }
 
 console.log(btnMobile);
+console.log(closingBtn);
 
-btnMobile.addEventListener('click', cliquei);
+
+btnMobile.addEventListener('click', handleClick);
+closingBtn.addEventListener('click', handleClick);
+
+
+
+
